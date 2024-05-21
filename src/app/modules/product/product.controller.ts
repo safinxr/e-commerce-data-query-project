703d9821c11a10aa689ef6f967f637335a368dcf
safinxr <safinxr@gmail.com>
2024-05-21 19:41:39 +0600

    zod validation added

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,25 +1,26 @@
 import { Request, Response } from "express";
 import { productServices } from "./product.service";
+import productSchemaValidation from "./product.validation";
 
-const createProduct = async(req:Request, res:Response) =>{
-    try{
-        const productData = req.body;
-        const result = await productServices.createdProductIntoDB(productData);
-         res.status(200).json({
-           success: true,
-           message: "Product is created successfully",
-           data: result,
-         });
-    }catch(err){
-      res.status(500).json({
-        success: false,
-        message: "Product creation failed",
-        data: err,
-      });
-    }
-
-}
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+    const zodValidation = productSchemaValidation.parse(productData)
+    const result = await productServices.createdProductIntoDB(zodValidation);
+    res.status(200).json({
+      success: true,
+      message: "Product is created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Product creation failed",
+      data: err,
+    });
+  }
+};
 
 export const productController = {
   createProduct,
-}
+};
---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const variantSchemaValidation = z.object({
+  type: z.string().min(1, "Type is required"),
+  value: z.string().min(1, "Value is required"),
+});
+
+const inventorySchemaValidation = z.object({
+  quantity: z.number().min(0, "Quantity must be a positive number"),
+  inStock: z.boolean(),
+});
+
+const productSchemaValidation = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  price: z.number().positive("Price must be a positive number"),
+  category: z.string().min(1, "Category is required"),
+  tags: z.array(z.string().min(1, "Tag cannot be empty")),
+  variants: z.array(variantSchemaValidation).min(1, "variants cannot be empty"),
+  inventory: inventorySchemaValidation,
+});
+
+
+export default productSchemaValidation;
