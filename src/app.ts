25355967ffa8bb7cfacfad22a662f5f83e830d1e
safinxr <safinxr@gmail.com>
2024-05-22 19:15:39 +0600

    not found route added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { productRoute } from "./app/modules/product/product.route";
+import {
+  error,
+  success,
+} from "./app/modules/product/product.config/try.catch.res";
 const app: Application = express();
 
 // parsers
@@ -9,8 +13,18 @@ app.use(cors());
 
 app.use("/api/products", productRoute);
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(500).send("Hello welcome to Ecommerce data api");
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    res.status(200).json(success("Hello welcome to E-commerce Data API ", {}));
+  } catch (err) {
+    res.status(500).json(error("Sorry something went wrong", err));
+  }
 });
+
+
+
+app.all("*", (req: Request, res: Response)=>{
+  res.status(400).json(error("Route is not found", {}));
+})
 
 export default app;
